cf630d8e0a56fe65ba5e3ad0808704eb22fac50b
m-atteo <m-atteo@hotmail.it>
2016-07-31 23:30:37 +0200

    rename

--- MVP-extention/script.js
+++
@@ -1,7 +0,0 @@
-//document.body.style.border = "5px solid red";
-
-
-browser.browserAction.onClicked.addListener((tab) => {
-	var action_url = "javascript:(function(){if(window.myBookmarklet!==undefined){myBookmarklet();}else{document.body.appendChild(document.createElement('script')).src='https://mvp.microsoft.com/Content/Scripts/bookmarklet.js?';}})();";
-	browser.tabs.update(tab.id, {url: action_url});
-});
